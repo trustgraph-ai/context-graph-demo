2cb0912b44e64999d294bc9d08b7676b5bed7d08
Cyber MacGeddon <cybermaggedon@gmail.com>
2026-02-25 13:24:28 +0000

    TG backend

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api/socket": {
+        //        target: "wss://broker.app.trustgraph.ai/",
+        target: "ws://localhost:8088/",
+        changeOrigin: true,
+        ws: true,
+        secure: false,
+        rewrite: (path) => path.replace("/api/socket", "/api/v1/socket"),
+      },
+      "/api/export-core": {
+        target: "http://localhost:8088/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (x) => x.replace("/api/export-core", "/api/v1/export-core"),
+      },
+      "/api/import-core": {
+        target: "http://localhost:8088/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (x) => x.replace("/api/import-core", "/api/v1/import-core"),
+      },
+    },
+  },
 })
