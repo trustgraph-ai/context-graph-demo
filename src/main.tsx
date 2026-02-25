9646e6f130a36eae3768750fadf8612857cb86e9
Cyber MacGeddon <cybermaggedon@gmail.com>
2026-02-25 13:33:32 +0000

    Plumbed in @trustgraph/react-state

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SocketProvider } from '@trustgraph/react-provider'
+import { NotificationProvider, NotificationHandler } from '@trustgraph/react-state'
 import './index.css'
 import App from './App'
 
+const queryClient = new QueryClient()
+
+const notificationHandler: NotificationHandler = {
+  success: (title: string) => console.log('[SUCCESS]', title),
+  error: (error: string) => console.error('[ERROR]', error),
+  warning: (warning: string) => console.warn('[WARNING]', warning),
+  info: (info: string) => console.info('[INFO]', info),
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <NotificationProvider handler={notificationHandler}>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </NotificationProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
